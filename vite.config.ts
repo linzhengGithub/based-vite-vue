f82abb340d1c78d310443613bdd19df1c7642f0d
linzheng <alidlg9045@126.com>
2022-10-15 00:00:06 +0800

    安装eslint & viteEslint

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,17 @@ import { defineConfig, normalizePath } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path';
 import Unocss from 'unocss/vite'
+// import viteEslint from 'vite-plugin-eslint';
 
 const variablePath = normalizePath(path.resolve('./src/variable.scss'));
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), Unocss()],
+  plugins: [
+    vue(),
+    Unocss(),
+    // viteEslint()
+  ],
   css: {
     preprocessorOptions: {
       scss: {
